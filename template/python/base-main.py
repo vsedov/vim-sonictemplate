9a5c9375ddb406942c16621d3ebf82c17241a488
vsedov <viv.sedov@hotmail.com>
2021-09-29 12:29:24 +0100

    added python test class

--- template/python/base-main.py
+++ template/python/base-main.py
@@ -1,9 +1,20 @@
-# vim: fileencoding=utf-8
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Viv Sedov
+#
+# File Name: {{_name_}}.py
+__author__ = "Viv Sedov"
+__email__ = "viv.sv@hotmail.com"
+
+import pyinspect as pi
 
 
-def main():
-	{{_cursor_}}
+def main() -> None:
+    {{_cursor_}}
 
-if __name__ == '__main__':
-	main()
 
+if __name__ == "__main__":
+    pi.install_traceback()
+    main()
---
+++ template/python/pytest-class.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Viv Sedov
+#
+# File Name: {{_name_}}
+__author__ = "Viv Sedov"
+__email__ = "viv.sv@hotmail.com"
+
+
+import pytest
+from termcolor import colored
+
+
+@pytest.fixture()
+def resource() -> {{_input_: var}}:
+    print(colored("Setup Outer Function Call", "magenta", "on_grey"))
+
+    yield {{_var_}}()
+
+    print(colored("Teardown Outer Function Call", "magenta", "on_grey"))
+
+
+@pytest.mark.usefixtures("resource")
+class TestArray:
+    @classmethod
+    def setup_class(cls) -> None:
+        print(colored(f"Setup Class : Start : class -> {cls.__name__} execution,green"))
+
+    @classmethod
+    def teardown_class(cls) -> None:
+        print(colored(f"Setup Class : Start : class -> {cls.__name__} execution,green"))
+
+    def test_case_1(self, resource: resource) -> None:
+        {{_cursor_}}
+        pass
