b9db752780fd80583dc05d532cba24dbcf3c3cdc
vsedov <viv.sedov@hotmail.com>
2021-09-29 12:36:41 +0100

    personal config added for classes etc for python

--- template/python/base-class.py
+++ template/python/base-class.py
@@ -1,6 +1,14 @@
-"""
-{{_name_}}
-"""
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Viv Sedov
+#
+# File Name: {{_name_}}.py
+__author__ = "Viv Sedov"
+__email__ = "viv.sv@hotmail.com"
+
+import pyinspect as pi
 
 
 class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(object):
@@ -8,3 +16,13 @@ class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(object):
 
     def __repr__(self):
         return
+
+
+def main() -> None:
+    {{_cursor_}}
+
+
+if __name__ == "__main__":
+    pi.install_traceback()
+    main()
+
--- template/python/snip-class.py
+++ template/python/snip-class.py
@@ -1,5 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © Viv Sedov
+#
+# File Name: {{_name_}}.py
+__author__ = "Viv Sedov"
+__email__ = "viv.sv@hotmail.com"
+import pyinspect as pi
+
+
 class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(object):
 	def __init__(self{{_cursor_}}):
 
     def __repr__(self):
         return
+
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pi.install_traceback()
+    main()
